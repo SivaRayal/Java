a1dfee0a7ca268d68855270de8b36e7b96ac00e4
Siva Rayal <kuruva.siva8055@gmail.com>
2025-02-25 11:09:16 +0530

    place holders for loop and control statements

---
+++ Java/src/main/com/ssk/javarefresher/basics/DecisionControlStatements.java
@@ -0,0 +1,13 @@
+package main.com.ssk.javarefresher.basics;
+
+public class DecisionControlStatements {
+    public static void main(String[] args) {
+        // if
+        // if-else
+        // nested if
+        // if-eles-if
+        // switch
+        // jump - break, continue, return
+
+    }
+}
---
+++ Java/src/main/com/ssk/javarefresher/basics/LoopIterations.java
@@ -0,0 +1,15 @@
+package main.com.ssk.javarefresher.basics;
+
+public class LoopIterations {
+    public static void main(String[] args) {
+        // while
+
+        // do-while
+
+        // for
+
+        // for-each
+
+
+    }
+}
